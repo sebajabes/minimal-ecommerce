2d0c038a919b076b16339e227b39651bc5ac1770
Jabes <jabesaribu@gmail.com>
2024-12-03 15:13:48 +0700

    Header, Hero Section, Product Section, LiveWire

---
+++ resources/views/livewire/product-section.blade.php
@@ -0,0 +1,14 @@
+<div class="px-10 md:px-20 sm:px-30 py-3">
+
+    {{-- BRAND NEW --}}
+    <h2 class="font-medium text-[20px] my-3">Brand New</h2>
+    <livewire:product-listing />
+
+    {{-- BRAND NEW --}}
+    <h2 class="font-medium text-[20px] my-3">Category</h2>
+    <livewire:product-listing />
+
+    {{-- BRAND NEW --}}
+    <h2 class="font-medium text-[20px] my-3">Others</h2>
+    <livewire:product-listing />
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/app.css')
+
+    <title>Minimal E-Commerce</title>
+
+
+</head>
+
+<body>
+    <livewire:header />
+    <livewire:hero-section />
+    <livewire:product-section />
+
+
+</body>
+
+</html>
